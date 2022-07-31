704556dbcfa9c070775df85015a341d9cd938a2a
abdelrahman <gadoua50@gmail.com>
2022-07-31 09:01:27 +0200

    add passport local strategy for authentication

--- app.js
+++ app.js
@@ -28,6 +28,27 @@ app.set("views", __dirname);
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    });
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
